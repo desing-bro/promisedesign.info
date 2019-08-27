374c5bedf403143c634be011abd96accde7042b5
eviljay <evgencherkashyn@gmail.com>
2019-08-27 15:26:28 +0300

    first commit

---
+++ cologne/pd_index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>promisedesign - 2009 - Cologne</title>
+</head>
+
+<frameset cols="237,*" frameborder="no" border="0" framespacing="0">
+  <frame src="leftFrame.php" name="leftFrame" scrolling="No" noresize="noresize" id="leftFrame" title="leftFrame" name="leftFrame" />
+  <frameset rows="*,40" frameborder="no" border="0" framespacing="0">
+<frame src="mainFrame.php" name="mainFrame" scrolling="no" id="mainFrame" title="mainFrame" name="mainFrame" />
+		<frame src="credits.php" name="bottomFrame" scrolling="No" noresize="noresize" id="bottomFrame" title="bottomFrame" name="bottomFrame" />
+	
+
+
+
+<noframes><body>
+</body>
+</noframes>
+
+
+
+</html>
